8ab223626b8dfaae7baea8bded18c0e870969f02
unknown <rareventar@up.edu.ph>
2020-07-17 14:25:19 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # leaveform
+
+For Windows
+
+1. npm install
+2. npm install mysql
+3. npm install mysqljs/mysql
+4. change credentials in database.js
+5. use leave.sql
+6. npm start
+7. use chrome or firefox and search "localhost:3000"
---
+++ routes/index.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: 'localhost', // Replace with your host name
+  user: 'root',      // Replace with your database username
+  password: 'root',      // Replace with your database password
+  database: 'leavedatabase' // // Replace with your database Name
+});
+con.connect(function(err) {
+  if (err) throw err;
+  // console.log('Database is connected successfully !a');
+});
+function getBusinessDatesCount(startDate, endDate) {
+  startDate = new Date(startDate);
+  endDate = new Date(endDate);
+  var count = 0;
+  var curDate = startDate;
+
+  while (curDate <= endDate) {
+    var dayOfWeek = curDate.getDay();
+    if(!((dayOfWeek == 6) || (dayOfWeek == 0)))
+      count++;
+      curDate.setDate(curDate.getDate() + 1);
+    }
+    return count;
+  }
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // console.log();
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/save', function(req,res){
+
+  let sql = "select * from user where name =  ?";
+  let sql1 = "update user set numLeave = ? where name = ?"
+  let sql2  = "insert into user (name, numLeave) values (?, ?)"
+  var count = 0;
+  console.log(req.body);
+  if(req.body.request == "whole" || req.body.request == "half"){
+    count  = 24 - 1;
+  }else{
+    count = 24 - getBusinessDatesCount(req.body.from, req.body.to);
+  }
+  console.log(count);
+  var name1 = req.body.name
+  let query = con.query(sql, req.body.name, (err, results) =>{
+    if(results.length > 0){
+      con.query(sql, req.body.name, (err, results) =>{
+        count = results[0].numLeave - count;
+        name =  results[0].name;
+        con.query(sql1, {count, name}, (err, results) => {
+          res.redirect('/');
+        });
+      })
+    }else{
+      con.query(sql2, [name1,count], (err, results) =>{
+        res.redirect('/');
+      });
+    }
+  });
+});
+
+module.exports = router;
